f65fa0b15e05df932f82294469b67670e2645fe8
Wictor <s184197@student.dtu.dk>
2021-03-26 15:03:49 +0100

    Changed deploy.js to target main file

--- deploy.js
+++ deploy.js
@@ -1,25 +1,28 @@
 const fs = require("fs");
 const Web3 = require("web3");
 const web3 = new Web3("http://localhost:8545");
-const bytecode = fs.readFileSync("./build/Test_sol_Test.bin");
-const abi = JSON.parse(fs.readFileSync("./build/Test_sol_Test.abi"));
+const bytecode = fs.readFileSync("./build/main_sol_Main.bin");
+const abi = JSON.parse(fs.readFileSync("./build/main_sol_Main.abi"));
 
+
+web3.eth.getChainId().then((res) => console.log);
 (async function () {
     const ganacheAccounts = await web3.eth.getAccounts();
-    const myWalletAddress = ganacheAccounts[0];
+
+    const myWalletAddress = ganacheAccounts[1];
 
     const myContract = new web3.eth.Contract(abi);
 
-    myContract.deploy({
-        data: bytecode.toString()
-    }).send({
-        from: myWalletAddress,
-        gas: 5000000
-    }).then((deployment) => {
-        console.log('FirstContract was successfully deployed!');
-        console.log('FirstContract can be interfaced with at this address:');
-        console.log(deployment.options.address);
-    }).catch((err) => {
-        console.error(err);
-    });
+    web3.eth.personal.unlockAccount(myWalletAddress, "", 10).then(() => {
+        myContract.deploy({
+            data: bytecode.toString()
+        }).send({
+            from: myWalletAddress,
+            gas: 5000000
+        }).then((deployment) => {
+            console.log(deployment.options.address);
+        }).catch((err) => {
+            console.error(err);
+        })
+    })
 })();
