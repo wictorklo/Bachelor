af32667e6a727579d9d063331384671e0ce8b3da
Wictor <s184197@student.dtu.dk>
2021-04-14 13:55:23 +0200

    AUTO COMPILER NOT WORKING WTF

--- Compile.sh
+++ Compile.sh
@@ -1,21 +1,14 @@
-solcjs --bin --abi -o ./build *.sol
-
+solcjs --bin --abi -o ./build Solidity/*.sol
 ADDR=$(node deploy.js | tail -n 1)
 OLDADDR=$(grep -e "const contractAddr" index.html | grep -o -E "0x[a-zA-Z0-9]*")
 echo $ADDR
 echo $OLDADDR
-
 sed -i "s/$OLDADDR/$ADDR/g" index.html
-
 echo "ADDRESS CHANGED"
-
 ABI=$(cat build/main_sol_Main.abi)
 OLDABI=$(grep -e "const ABI =" index.html | grep -o -E "\[.+\]")
-
 echo $ABI
 echo " --- "
 echo $OLDABI
-
 sed -i "s/$OLDABI/$ABI/g" index.html
-
 echo "ABI CHANGED"
--- deploy.js
+++ deploy.js
@@ -1,9 +1,34 @@
 const fs = require("fs");
+const solc = require("solc");
+const path = require("path");
 const Web3 = require("web3");
 const web3 = new Web3("http://localhost:8545");
 const bytecode = fs.readFileSync("./build/main_sol_Main.bin");
 const abi = JSON.parse(fs.readFileSync("./build/main_sol_Main.abi"));
 
+const mainPath = path.resolve(__dirname, "Solidity", "main.sol");
+const src = fs.readFileSync(mainPath, "UTF-8");
+let cc = solc.compile(src, 1);
+
+const input = {
+    language: "Solidity",
+    sources: {
+        "main.sol": {
+            content: src,
+        },
+    },
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["*"],
+            },
+        },
+    },
+};
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+
+/*
 var mainContract;
 web3.eth.getChainId().then((res) => console.log);
 var accounts;
@@ -43,7 +68,6 @@ async function setupMain () {
             names.forEach(name => {
                 let bc = fs.readFileSync("./build/" + name + ".bin");
                 let rawABI = fs.readFileSync("./build/" + name + ".abi", "utf8");
-                console.log(rawABI);
                 let ABI = JSON.parse(rawABI);
                 const myContract = new web3.eth.Contract(ABI);
                 web3.eth.personal.unlockAccount(myWalletAddress, "", 10).then(() => {
@@ -66,4 +90,4 @@ async function setupMain () {
     } catch (err) {
         console.error(err);
     }
-})();
+})();*/
